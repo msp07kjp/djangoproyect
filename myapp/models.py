1c23e07bbcbfc2629d1e51079db2b3b70cd572af
Kevin Jimenez <kevin.stiven.jimenez@gmail.com>
2023-07-26 18:19:03 -0500

    Modelos y Base de datos

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Proyecto(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=200)
+    fecha_creacion = models.DateField()
+    fecha_entrega = models.DateField()
+    estado = models.CharField(max_length=20)
+    def __str__(self):
+        return self.nombre
+class Task(models.Model):
+    id = models.AutoField(primary_key=True)
+    proyecto = models.ForeignKey(Proyecto, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=200)
+    fecha_creacion = models.DateField()
+    fecha_entrega = models.DateField()
+    estado = models.CharField(max_length=20)
+    def __str__(self):
+        return self.nombre
