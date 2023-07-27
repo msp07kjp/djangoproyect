53b318c94ad6da6941ca886eb6e2fb1c07d3b90d
Kevin Jimenez <kevin.stiven.jimenez@gmail.com>
2023-07-27 00:32:44 -0500

    Params y Models

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.hello, name='hello'),
+    path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('hello/<str:username>', views.hello, name='hello'),
+    path('proyect/', views.proyect, name='proyect'),
+    path('task/<int:id>', views.task, name='task'),
+
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponse, JsonResponse
+from .models import Proyecto, Task
+from django.shortcuts import get_object_or_404
 # Create your views here.
-def hello(request):
-    return HttpResponse("Hello world ! desde Django")
+def index(request):
+    return HttpResponse("index desde Django")
+def hello(request,username):
+    return HttpResponse("<h1>hello desde Django %s</h1>" % username)
 def about(request):
     return HttpResponse("about")
+def proyect(request):
+    proyects = list(Proyecto.objects.values())
+    return JsonResponse(proyects, safe=False)
+def task(request,id):
+    #task= Task.objects.get(id=id)
+    task = get_object_or_404(Task, id=id)
+    return HttpResponse("task: %s" % task.nombre)
