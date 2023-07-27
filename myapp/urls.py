ce7689e81778710f478e708fdf68a47f5e0e5303
Kevin Jimenez <kevin.stiven.jimenez@gmail.com>
2023-07-27 11:56:57 -0500

    Template inheritance

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
-    path('hello/<str:username>', views.hello, name='hello'),
     path('proyect/', views.proyect, name='proyect'),
     path('task/', views.task, name='task'),
+    path('about/', views.about, name='about'),
+    path('hello/<str:username>', views.hello, name='hello'),
 
 ]
